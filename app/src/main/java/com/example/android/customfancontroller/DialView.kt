2c66b9dd6033e9758031132d01d8a3b9619ee4f0
Knut Valen <knut.valen@icloud.com>
2020-12-18 12:46:27 +0100

    Create DialView.kt

---
+++ app/src/main/java/com/example/android/customfancontroller/DialView.kt
@@ -0,0 +1,18 @@
+package com.example.android.customfancontroller
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+private enum class FanSpeed(val label: Int) {
+    OFF(R.string.fan_off),
+    LOW(R.string.fan_low),
+    MEDIUM(R.string.fan_medium),
+    HIGH(R.string.fan_high);
+}
+
+class DialView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+}
