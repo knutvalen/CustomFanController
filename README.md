2020f7da7fb5d24626c8eee257aee5fef550d6df
Knut Valen <knut.valen@icloud.com>
2020-12-18 14:09:12 +0100

    finish painting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CustomFanController
+[![Build Status](https://app.bitrise.io/app/58a2c8d78b425e71/status.svg?token=3gdo4ldIX1hOyxqTl4ygSA)](https://app.bitrise.io/app/58a2c8d78b425e71)
+
+This is the app for learning how to create custom views by the [Android App Development in Kotlin course at Udacity](https://www.udacity.com/course/developing-android-apps-with-kotlin--ud9012).
--- app/src/main/java/com/example/android/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/android/customfancontroller/DialView.kt
@@ -1,10 +1,7 @@
 package com.example.android.customfancontroller
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.PointF
-import android.graphics.Typeface
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import kotlin.math.cos
@@ -52,6 +49,25 @@ class DialView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        // Set dial background color to green if selection not off.
+        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+
+        // Draw the dial.
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
+
+        // Draw the indicator circle.
+        val markerRadius = radius + RADIUS_OFFSET_INDICATOR
+        pointPosition.computeXYForSpeed(fanSpeed, markerRadius)
+        paint.color = Color.BLACK
+        canvas.drawCircle(pointPosition.x, pointPosition.y, radius/12, paint)
+
+        // Draw the text labels.
+        val labelRadius = radius + RADIUS_OFFSET_LABEL
+        for (i in FanSpeed.values()) {
+            pointPosition.computeXYForSpeed(i, labelRadius)
+            val label = resources.getString(i.label)
+            canvas.drawText(label, pointPosition.x, pointPosition.y, paint)
+        }
     }
 
 }
